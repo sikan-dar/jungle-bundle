8b5cd0c080230945504f695bc0b64ac2528a1fa0
theExicuter <sikandarali@ieee.org>
2020-05-10 18:29:25 +0530

    Here is a small dynamic Web Page

---
+++ main.js
@@ -0,0 +1,26 @@
+
+function wolfSpeaking(){
+    const audio = new Audio("./sounds/wolf.mp3");
+    audio.play();
+}
+function lionSpeaking(){
+    const audio = new Audio("./sounds/lion.mp3");
+    audio.play();
+}
+function elephantSpeaking(){
+    const audio = new Audio("./sounds/elephant.mp3");
+    audio.play();
+}
+function crocoSpeaking(){
+    const audio = new Audio("./sounds/crocodile.mp3");
+    audio.play();
+}
+
+const audio = new Audio("./sounds/junglemusic.mp3");
+ 
+function jungleMusic(){
+    audio.play();
+}
+function jungleMusic1(){
+    audio.pause();
+}
